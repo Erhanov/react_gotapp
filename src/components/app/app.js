f52b26ae9a7c21121824760678ba1a1d6382e1cc
Azat <aza.er.01@mail.ru>
2019-06-20 22:48:33 +0600

    8

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import {Col, Row, Container, Button} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 import GotServices from '../../services/gotService';
+import './toggle.css';
 
+const toggleRandomCharacter = () => {
+    let randomBlock = document.querySelector('.offset-lg-0');
+    randomBlock.style.display = (randomBlock.style.display === 'block') ? 'none' : 'block'
+}
 
 const App = () => {
     return (
@@ -17,8 +22,10 @@ const App = () => {
                 <Row>
                     <Col lg={{size: 5, offset: 0}}>
                         <RandomChar/>
+                        
                     </Col>
                 </Row>
+                <Button className='bottomMargin' outline color="primary" onClick={toggleRandomCharacter}>Toggle Random Character</Button>
                 <Row>
                     <Col md='6'>
                         <ItemList />
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -47,7 +47,7 @@ export default class RandomChar extends Component {
     }
 
     updateChar() {
-        const id = Math.floor(Math.random()*140 + 2500000);
+        const id = Math.floor(Math.random()*140 + 25);
         this.gotService.getCharacter(id)
             .then(this.onCharLoaded)
             .catch(this.onError);
